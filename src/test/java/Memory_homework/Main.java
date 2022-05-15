0cb837423174a3240242f1f955f7deb2554e9b3f
Користувач <dimabekar8@gmail.com>
2022-05-15 16:55:00 +0300

    Homework Memory---RegEx

---
+++ src/test/java/Memory_homework/Main.java
@@ -0,0 +1,10 @@
+package Memory_homework;
+
+public class Main {
+    public static void main(String[] args) {
+    One one=new One();
+//    one.hometask_1();
+//    one.hometask_2();
+        one.hometask_3();
+    }
+}
