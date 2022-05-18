4624bf89486533f384514d9cda2529359e020257
Користувач <dimabekar8@gmail.com>
2022-05-18 13:11:07 +0300

    Homework for Array

---
+++ src/test/java/List/Main.java
@@ -0,0 +1,10 @@
+package List;
+
+public class Main {
+    public static void main(String[] args) {
+    One one=new One("a");
+    one.hometask_1();
+    one.hometask_2();
+    one.hometask_3();
+    }
+}
