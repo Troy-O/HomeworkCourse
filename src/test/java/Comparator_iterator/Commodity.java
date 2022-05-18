24c5563fc77e7008e12ffa264a30ad14a9ae8805
Користувач <dimabekar8@gmail.com>
2022-05-18 15:12:27 +0300

    Homework for Comparator_Iterator

---
+++ src/test/java/Comparator_iterator/Commodity.java
@@ -0,0 +1,37 @@
+package Comparator_iterator;
+
+import java.util.Scanner;
+
+public class Commodity {
+    void hometask_1(){
+        System.out.println("Введи слово:");
+        Scanner scanner=new Scanner(System.in);
+        int x=scanner.nextInt();
+        switch (x){
+            case 1:
+                add_item();
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+
+        }
+    }
+
+    void  add_item(){
+
+    }
+}
---
+++ src/test/java/Comparator_iterator/Main.java
@@ -0,0 +1,8 @@
+package Comparator_iterator;
+
+public class Main {
+    public static void main(String[] args) {
+            Commodity com=new Commodity();
+
+    }
+}
